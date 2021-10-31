d65f8229bc10850aacd214a9079b076550333940
MashodRana <mashod0rana@gmail.com>
2021-10-31 22:40:31 +0600

    updated few code

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const {MongoClient, ObjectId} = require('mongodb');
+const { MongoClient, ObjectId } = require('mongodb');
 const { query } = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -14,8 +14,8 @@ const mongoUri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD
 
 const client = new MongoClient(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-const run = async ()=>{
-    try{
+const run = async () => {
+    try {
         await client.connect();
         console.log("Database connection succesfull.");
 
@@ -23,31 +23,31 @@ const run = async ()=>{
         const tourPlanCollection = database.collection('tour_plans');
         const orderCollection = database.collection('order');
 
-        app.get('/tour-plans', async(req, res)=>{
+        app.get('/tour-plans', async (req, res) => {
             const crusor = tourPlanCollection.find({});
             const tourPlans = await crusor.toArray();
             res.send(tourPlans);
         });
 
-        app.get('/tour-plan-detail/:planId', async(req, res)=>{
+        app.get('/tour-plan-detail/:planId', async (req, res) => {
             const planId = req.params.planId;
-            const query = {_id:ObjectId(planId)}
+            const query = { _id: ObjectId(planId) }
             const cursor = await tourPlanCollection.findOne(query);
             // console.log(cursor);
             res.json(cursor)
         })
 
-        app.post('/add-order', async(req, res)=>{
+        app.post('/add-order', async (req, res) => {
             const orderData = req.body
             console.log(orderData)
             const cursor = await orderCollection.insertOne(orderData);
-            console.log('cursor result ',cursor)
-            res.json({message:"We have received your information. Soon, we will contact with you."})
+            console.log('cursor result ', cursor)
+            res.json({ message: "We have received your information. Soon, we will contact with you." })
         })
 
-        app.get('/my-orders/:userEmail', async(req, res)=>{
+        app.get('/my-orders/:userEmail', async (req, res) => {
             const userEmail = req.params.userEmail;
-            const query = {email:userEmail};
+            const query = { email: userEmail };
             console.log(query)
             const cursor = orderCollection.find(query);
             const orders = await cursor.toArray();
@@ -55,22 +55,29 @@ const run = async ()=>{
 
         })
 
-        app.delete('/remove-orders/:orderId', async(req,res)=>{
+        app.delete('/remove-order/:orderId', async (req, res) => {
             const orderId = req.params.orderId;
-            const query = {_id:ObjectId(orderId)};
+            const query = { _id: ObjectId(orderId) };
             const cursor = await orderCollection.deleteOne(query);
-            res.json({message:"your order is cancled. Pleae stay with us."});
-        })
+            res.json({ message: "your order is cancled. Pleae stay with us." });
+        });
+
+        app.get("/all-orders", async (req, res) => {
+            const cursor = orderCollection.find({});
+            const allOrders = await cursor.toArray();
+            res.json(allOrders);
+        });
+
     }
-    finally{
+    finally {
         // await client.close();
     }
 };
 
 run().catch(console.error.dir);
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.send('True Tour mentor backend');
 })
 
-app.listen(port, ()=>console.log('server is running on port:', port));
+app.listen(port, () => console.log('server is running on port:', port));
